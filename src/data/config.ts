d36366d79c3e6285d059cf5b94d4f1bc815707b7
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-10-12 23:04:23 +0300

    NavBar component was implemented

---
+++ src/data/config.ts
@@ -0,0 +1,18 @@
+export const navData = [
+  {
+    href: "#about",
+    caption: "Про агенцію",
+  },
+  {
+    href: "#services",
+    caption: "Послуги",
+  },
+  {
+    href: "#cases",
+    caption: "Кейси",
+  },
+  {
+    href: "#team",
+    caption: "Команда",
+  },
+];
