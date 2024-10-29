a44eafedaf9379105b1bdbefd052d9413531a35c
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-10-29 22:24:15 +0200

    team images were added

---
+++ src/data/team.ts
@@ -0,0 +1,28 @@
+export interface ITeamCard {
+  fullname: string;
+  position: string;
+  imageFilename: string;
+}
+
+export default [
+  {
+    fullname: "Роман",
+    position: "Co-founder, директор",
+    imageFilename: "roman",
+  },
+  {
+    fullname: "Белла",
+    position: "Co-founder, проджект",
+    imageFilename: "bella",
+  },
+  {
+    fullname: "Сніжана",
+    position: "UI/UX, графічниц дизайнер",
+    imageFilename: "snizhana",
+  },
+  {
+    fullname: "Лєна",
+    position: "СММ",
+    imageFilename: "lena",
+  },
+];
