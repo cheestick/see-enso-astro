13af8e3e283db7778a6d5c565adacffb2696951a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-12-05 11:34:09 +0200

    services data was added

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,11 @@
 import mainNavData from "./nav";
 import casesData from "./cases";
+import seServices from "./services";
 import team from "./team";
 
 export default {
   mainNavData,
   casesData,
+  seServices,
   team,
 };
