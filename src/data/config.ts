58bad8d27bad8cdf7e03d2360fed610aa8f2db74
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-12-09 11:47:28 +0200

    contact info data was added

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,11 @@
 import mainNavData from "./nav";
 import casesData from "./cases";
 import team from "./team";
+import { seContactInfo } from "./contactInfo";
 
 export default {
   mainNavData,
   casesData,
   team,
+  seContactInfo,
 };
---
+++ src/data/contactInfo.ts
@@ -0,0 +1,38 @@
+export interface ISEContactInfo {
+  icon: string;
+  caption: string;
+  link: string;
+}
+
+export const seContactInfo: ISEContactInfo[] = [
+  {
+    icon: "",
+    caption: "tiktok",
+    link: "https://tiktok.com",
+  },
+  {
+    icon: "",
+    caption: "youtube",
+    link: "https://youtube.com",
+  },
+  {
+    icon: "",
+    caption: "instagram",
+    link: "https://instagram.com",
+  },
+  {
+    icon: "",
+    caption: "telegram",
+    link: "https://t.me/See_Enso",
+  },
+  {
+    icon: "",
+    caption: "+380 99 967 77 76",
+    link: "tel:+380999677776",
+  },
+  {
+    icon: "",
+    caption: "email",
+    link: "mail:example@example.com",
+  },
+];
