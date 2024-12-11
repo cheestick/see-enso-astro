8a7579012884d8af1b0956c79679b09dc5bca52e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-12-11 21:20:19 +0200

    footer contacts was fixed

--- src/data/contactInfo.ts
+++ src/data/contactInfo.ts
@@ -1,14 +1,13 @@
 export interface ISEContactInfo {
   icon: string;
   caption: string;
-  link: string;
+  link?: string;
 }
 
 export const seContactInfo: ISEContactInfo[] = [
   {
     icon: "/images/location-pin.svg",
     caption: "Україна, Київ",
-    link: "",
   },
   {
     icon: "/images/phone-call.svg",
@@ -41,3 +40,40 @@ export const seContactInfo: ISEContactInfo[] = [
   //   link: "https://tiktok.com",
   // },
 ];
+
+export const seContacts: Record<string, ISEContactInfo> = {
+  location: {
+    icon: "/images/location-pin.svg",
+    caption: "Україна, Київ",
+  },
+  phone: {
+    icon: "/images/phone-call.svg",
+    caption: "+380 99 967 77 76",
+    link: "+380999677776",
+  },
+  telegram: {
+    icon: "/images/telegram.svg",
+    caption: "telegram",
+    link: "https://t.me/See_Enso",
+  },
+  instagram: {
+    icon: "/images/instagram.svg",
+    caption: "instagram",
+    link: "https://www.instagram.com/see.enso",
+  },
+  youtube: {
+    icon: "/images/youtube.svg",
+    caption: "youtube",
+    link: "https://youtube.com",
+  },
+  email: {
+    icon: "/images/email.svg",
+    caption: "enso.filmmaker@gmail.com",
+    link: "enso.filmmaker@gmail.com",
+  },
+  tiktok: {
+    icon: "/images/tiktok.svg",
+    caption: "tiktok",
+    link: "https://tiktok.com",
+  },
+};
