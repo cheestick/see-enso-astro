365617d5ef34516ce09d332c00b684b12a8eff1e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2024-12-11 15:25:18 +0200

    contact list icons were added. contact info file was updated

--- src/data/contactInfo.ts
+++ src/data/contactInfo.ts
@@ -6,33 +6,38 @@ export interface ISEContactInfo {
 
 export const seContactInfo: ISEContactInfo[] = [
   {
-    icon: "",
-    caption: "tiktok",
-    link: "https://tiktok.com",
+    icon: "/images/location-pin.svg",
+    caption: "Україна, Київ",
+    link: "",
   },
   {
-    icon: "",
-    caption: "youtube",
-    link: "https://youtube.com",
-  },
-  {
-    icon: "",
-    caption: "instagram",
-    link: "https://instagram.com",
-  },
-  {
-    icon: "",
-    caption: "telegram",
-    link: "https://t.me/See_Enso",
-  },
-  {
-    icon: "",
+    icon: "/images/phone-call.svg",
     caption: "+380 99 967 77 76",
     link: "tel:+380999677776",
   },
   {
-    icon: "",
-    caption: "email",
-    link: "mail:example@example.com",
+    icon: "/images/telegram.svg",
+    caption: "telegram",
+    link: "https://t.me/See_Enso",
   },
+  {
+    icon: "/images/instagram.svg",
+    caption: "instagram",
+    link: "https://www.instagram.com/see.enso",
+  },
+  {
+    icon: "/images/youtube.svg",
+    caption: "youtube",
+    link: "https://youtube.com",
+  },
+  {
+    icon: "/images/email.svg",
+    caption: "enso.filmmaker@gmail.com",
+    link: "mail:enso.filmmaker@gmail.com",
+  },
+  // {
+  //   icon: "/images/tiktok.svg",
+  //   caption: "tiktok",
+  //   link: "https://tiktok.com",
+  // },
 ];
