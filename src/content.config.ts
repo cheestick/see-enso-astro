114a204852304f0b1a9ba82824f30eb394c8c065
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2025-04-14 18:58:24 +0300

    case cards were refactored for Images astro component. .yml content collection was implemented named cases.yml

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content";
+import { file } from "astro/loaders";
+
+const cases = defineCollection({
+  loader: file("src/data/cases.yml"),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().min(3),
+      description: z.string().min(5),
+      thumbUrl: image(),
+      tags: z.array(z.string()),
+      link: z.string().optional(),
+    }),
+});
+
+export const collections = {
+  cases,
+};
--- src/data/cases.ts
+++ src/data/cases.ts
@@ -1,5 +1,5 @@
 export interface ICaseCard {
-  heading: string;
+  title: string;
   description: string;
   thumbUrl: string;
   tags: string[];
@@ -8,7 +8,7 @@ export interface ICaseCard {
 
 export default [
   {
-    heading: "СММ ВЕДЕННЯ  СОЦ. МЕРЕЖ ФОНДУ “БЛАГОМАЙ”",
+    title: "СММ ВЕДЕННЯ  СОЦ. МЕРЕЖ ФОНДУ “БЛАГОМАЙ”",
     description:
       "Повне СММ ведення від контент плану до постингу. Монтаж Reels, створення візуального контенту, копірайтинг дописів, таргетована реклама (від стратегії до якісних показників)",
     thumbUrl: "/images/cases/01.png",
@@ -16,16 +16,16 @@ export default [
     link: "https://www.instagram.com/blagomayfund/",
   },
   {
-    heading:
+    title:
       "ТАРГЕТ + СТВОРЕННЯ ДОПИСІВ ДЛЯ СОЦ МЕРЕЖ “СОС ДИТЯЧІ МІСТЕЧКА УКРАЇНА”",
     description:
       "Написання текстів та дизайн дописів, зйомка та монтаж Reels, одного з проектів фонду, таргетована реклама соцмереж вцілому.",
-    thumbUrl: "/images/cases/02.png",
+    thumbUrl: "/images//cases/02.png",
     tags: ["Дизайн", "Створення дописів", "Таргет"],
     link: "https://www.instagram.com/sos_cv_ukraine/",
   },
   {
-    heading: "ПРОВЕДЕННЯ СЕРІЇ ONLINE ВЕБІНАРІВ",
+    title: "ПРОВЕДЕННЯ СЕРІЇ ONLINE ВЕБІНАРІВ",
     description:
       "Створення креативів для таргету та обкладинок в соц. мережі та на ютуб Організація, трансляція та технічний  супровід лекторів та вебінарів.",
     thumbUrl: "/images/cases/03.png",
@@ -33,7 +33,7 @@ export default [
     link: "https://www.youtube.com/playlist?list=PLCY4teqJXrLcbd2_fqkZTrlah8vm83Y2f",
   },
   {
-    heading: "ПРОМОЦІЯ СЕРІЇ ONLINE ВЕБІНАРІВ",
+    title: "ПРОМОЦІЯ СЕРІЇ ONLINE ВЕБІНАРІВ",
     description:
       "Промоція в соц мережах, таргетована реклама, реєстраційні форми, розсилка запрошень та  презентаційних матеріалів.\nЗбільшення бази нових  учасників в чотири рази!",
     thumbUrl: "/images/cases/04.png",
@@ -41,7 +41,7 @@ export default [
     link: "",
   },
   {
-    heading: "ВЕДЕННЯ РОЗДІЛУ ПРОЄКТУ НА САЙТІ “СОС ДИТЯЧІ МІСТЕЧКА УКРАЇНА”",
+    title: "ВЕДЕННЯ РОЗДІЛУ ПРОЄКТУ НА САЙТІ “СОС ДИТЯЧІ МІСТЕЧКА УКРАЇНА”",
     description:
       "Ведення та наповнення розділу проєкту на сайті (написання статей, підбір контенту, додавання новин та вебінарів, ведення сторінки проєкту в цілому).",
     thumbUrl: "/images/cases/05.png",
